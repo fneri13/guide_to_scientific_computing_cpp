74890d39f9b0a7fa7caee0dce8329f69351ec3c5
Francesco neri <95149635+fneri13@users.noreply.github.com>
2022-11-21 12:34:28 +0100

    Added an exercise

---
+++ chapter01/ex_1.5/ex_15.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    string Babbo;
+    string Mamma;
+    string Sorella;
+    cout<<"input name of Dad: ";
+    cin>>Babbo;
+    cout<<"input name of Mom: ";
+    cin>>Mamma;
+    cout<<"input name of sister: ";
+    cin>>Sorella;
+
+    cout<< endl;
+    cout<<"My dad name is "<<Babbo<<", my mom is called "<<Mamma<<", and my sister's name is "<<Sorella<<endl;
+
+    return 0;
+}
