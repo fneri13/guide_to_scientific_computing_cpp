6e49f8ffb9fcdf02bbfe6e160f01a48c6aa21be5
Francesco neri <95149635+fneri13@users.noreply.github.com>
2022-11-29 10:59:55 +0100

    comments

--- chapter_11_mpi/example_01/MpiHelloWorld.cpp
+++ chapter_11_mpi/example_01/MpiHelloWorld.cpp
@@ -3,8 +3,8 @@
 
 int main(int argc, char* argv[]) {
     MPI::Init(argc, argv);
-    int num_procs = MPI::COMM_WORLD.Get_size();
-    int rank = MPI::COMM_WORLD.Get_rank(); 
+    int num_procs = MPI::COMM_WORLD.Get_size(); //returns how many processes are initialized (with mpirun command)
+    int rank = MPI::COMM_WORLD.Get_rank(); //returns the first index of the list of process available
     std::cout << "Hello world from process " << rank
               << " of " << num_procs << "\n";
     MPI::Finalize();
--- chapter_11_mpi/example_02/SeriesSummationParallel.cpp
+++ chapter_11_mpi/example_02/SeriesSummationParallel.cpp
@@ -7,9 +7,9 @@
 int main(int argc, char* argv[]){
     int max_n = 1000;
     double sum = 0;
-    MPI::Init(argc, argv);
-    int num_procs = MPI::COMM_WORLD.Get_size();
-    int rank = MPI::COMM_WORLD.Get_rank();
+    MPI::Init(argc, argv); //initialize a parallel execution on argc processes
+    int num_procs = MPI::COMM_WORLD.Get_size(); //how many processes
+    int rank = MPI::COMM_WORLD.Get_rank(); //get the first index of these processes
     
     for (int n=rank; n<max_n; n+=num_procs){
         double temp = 1.0/(2.0*((double)(n))+1.0);
@@ -23,13 +23,12 @@ int main(int argc, char* argv[]){
         }
     }
     double global_sum;
-    MPI::COMM_WORLD.Reduce(&sum, &global_sum, 1,
-    MPI::DOUBLE, MPI::SUM, 0);
+    MPI::COMM_WORLD.Reduce(&sum, &global_sum, 1,MPI::DOUBLE, MPI::SUM, 0); //tranfer info from all to one process
     if (rank == 0){
     std::cout << "Pi is about " << 4.0*global_sum
         << " with error " << 4.0*global_sum-M_PI
         << "\n";
     }
-    MPI::Finalize();
+    MPI::Finalize(); //close the parallelization process
     return 0;
     }
